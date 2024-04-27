9e6e81b5c72733e3a397411f96ba34589107a550
root <root@ip-172-31-15-48.ap-south-1.compute.internal>
2024-04-27 02:49:12 +0000

    SampleText.java

---
+++ src/test/java/com/visualpathit/account/controllertest/SampleTest.java
@@ -0,0 +1,15 @@
+
+package com.visualpathit.account.controllerTest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SampleTest {
+	@Test
+	public void SampleTestHappyFlow(){
+		assertEquals("Hello".length(), 5);
+	}
+
+}
+
