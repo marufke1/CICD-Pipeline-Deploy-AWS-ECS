315481cc69f332365662a6fd1f47d5459bdb996d
root <root@ip-172-31-15-48.ap-south-1.compute.internal>
2024-04-27 03:09:31 +0000

    RoleRepository.java

---
+++ main/java/com/visualpathit/account/Repository/RoleRepository.java
@@ -0,0 +1,11 @@
+
+
+package com.visualpathit.account.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.visualpathit.account.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+}
+
